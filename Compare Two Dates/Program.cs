b887728adab2194f5d0dfbbf4341dc248d52ab9b
bougounika <31166561+bougounika@users.noreply.github.com>
2017-08-26 11:55:46 +0400

    Add project files.

---
+++ Compare Two Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * C# Program to Campare Two Dates
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compare_Two_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime sd = new DateTime(2010, 10, 12);
+            Console.WriteLine("Starting Date : {0}", sd);
+            DateTime ed = sd.AddDays(10);
+            Console.WriteLine("Ending Date   : {0}", ed);
+            if (sd < ed)
+                Console.WriteLine("{0} Occurs Before {1}", sd, ed);
+            Console.Read();
+            return;
+        }
+    }
+}
